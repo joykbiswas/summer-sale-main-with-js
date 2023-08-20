c62bbe824b2c4ba4e0b622fa8428b5f0897efce5
joykbiswas <joykumarbiswas100@gmail.com>
2023-08-21 02:08:25 +0600

    price calculate

---
+++ price-calculator.js
@@ -0,0 +1,57 @@
+
+let totalPrice = 0;
+
+function handleClickCard(target) {
+  const selectItemContainer = document.getElementById("selected-item");
+  const button = document.getElementById("myButton");
+  const itemName = target.childNodes[3].children[1].innerText;
+
+  const li = document.createElement("li");
+  li.innerText = itemName;
+  selectItemContainer.appendChild(li);
+
+  const price = target.childNodes[3].children[2].innerText.split(" ")[0];
+  console.log(price);
+
+  totalPrice = parseFloat(totalPrice) + parseFloat(price);
+  document.getElementById("total-price").innerText = totalPrice;
+  if (totalPrice > 0) {
+    button.removeAttribute("disabled");
+  }
+  
+ return totalPrice;
+  
+}
+
+
+function calculateTotal(){
+  const inputFieldCoupon = document.getElementById('input-field');
+  
+  const applyBtn = document.getElementById('btn-apply')
+  const inputField = inputFieldCoupon.value;
+
+ 
+
+  
+ 
+  if(inputField === "SELL200" && totalPrice >= 200) {
+    
+    const amount = totalPrice;
+    const discount = 0.20;
+
+    const discountAmount = amount * discount;
+    const discountTotal = discountAmount.toFixed(2)
+    document.getElementById('discount').innerText = discountTotal;
+     
+    const total = totalPrice - discountTotal;
+    const totalAmount = total.toFixed(2)
+    document.getElementById('total').innerText = totalAmount;
+    
+    
+  }else{
+    alert (" invalid ");
+  }
+  
+  
+}
+
